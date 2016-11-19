399e9285402cfa5bb646c9d98ef2465271a30710
Jorge Elizondo <Jelizondolevy.s@gmail.com>
2016-11-19 15:08:04 -0600

    adds gets

--- models/Ejercicio.java
+++ models/Ejercicio.java
@@ -8,5 +8,35 @@ import java.sql.SQLException;
 public class Ejercicio{
     PreparedStatement stmt;
 
-      
+    public String getPregunta(int idEjercicio, Connection con){
+      try{
+          String query = "SELECT pregunta FROM Ejercicio WHERE Id = ?";
+          stmt = con.prepareStatement(query);
+          stmt.setInt(1, idEjercicio);
+
+          ResultSet rs = stmt.executeQuery();
+          if (rs.next()) { ///Va al primer registro si lo hay
+             String sPregunta = rs.getString ("pregunta");
+             rs.close();
+             return(sPregunta);
+           }
+        } catch (SQLException e) {}
+       return "";
+    }
+
+    public String getRespuesta(int idEjercicio, Connection con){
+      try{
+          String query = "SELECT respuesta FROM Ejercicio WHERE Id = ?";
+          stmt = con.prepareStatement(query);
+          stmt.setInt(1, idEjercicio);
+
+          ResultSet rs = stmt.executeQuery();
+          if (rs.next()) { ///Va al primer registro si lo hay
+             String sRespuesta = rs.getString ("respuesta");
+             rs.close();
+             return(sRespuesta);
+           }
+        } catch (SQLException e) {}
+       return "";
+    }
 }
