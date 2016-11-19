ae4f9aee5f467d8b56e3c43cb656487e4d3d6717
Jorge Elizondo <Jelizondolevy.s@gmail.com>
2016-11-19 15:21:38 -0600

    finishes ejercicio

--- models/Ejercicio.java
+++ models/Ejercicio.java
@@ -39,4 +39,41 @@ public class Ejercicio{
         } catch (SQLException e) {}
        return "";
     }
+
+    public Boolean validarRespuesta(int idEjercicio, String sRespuesta, Connection con){
+      try{
+          String query = "SELECT respuesta FROM Ejercicio WHERE Id = ?";
+          stmt = con.prepareStatement(query);
+          stmt.setInt(1, idEjercicio);
+
+          ResultSet rs = stmt.executeQuery();
+          if (rs.next()) { ///Va al primer registro si lo hay
+             String sRes = rs.getString ("respuesta");
+
+             if (sRes != sRespuesta) {
+               return false;
+             }
+             rs.close();
+
+           }
+        } catch (SQLException e) {return false;}
+       return true;
+    }
+
+    public int sacarCalificacion(int idEjercicio, Connection con){
+      try{
+          String query = "SELECT calificacion FROM Ejercicio WHERE Id = ?";
+          stmt = con.prepareStatement(query);
+          stmt.setInt(1, idEjercicio);
+
+          ResultSet rs = stmt.executeQuery();
+          if (rs.next()) { ///Va al primer registro si lo hay
+             int iCal = rs.getInt ("calificacion");
+             rs.close();
+             return iCal;
+
+           }
+        } catch (SQLException e) {return 0;}
+       return 0;
+    }
 }
