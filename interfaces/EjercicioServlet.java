7a0f5abca23a409c4170e5368ff6d2133aa61c3d
Jorge Elizondo <Jelizondolevy.s@gmail.com>
2016-11-22 16:30:52 -0600

    fixes issues

--- interfaces/EjercicioServlet.java
+++ interfaces/EjercicioServlet.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import controllers.ControlLectura;
+import controllers.ControlEjercicio;
 
 @WebServlet(urlPatterns = "/Ejercicio",
   initParams = {
@@ -23,7 +23,7 @@ import controllers.ControlLectura;
   }
 )
 
-public class LecturasServlet extends HttpServlet {
+public class EjercicioServlet extends HttpServlet {
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
      doPost(request,response);
